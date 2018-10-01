8945f5431c7060616f0ee574c9da1053a9a0b012
Nareshkc190 <nareshkc1.9.0@gmail.com>
2018-10-01 10:39:55 -0400

    added servlet

---
+++ src/com/servlets/DatabaseConnection.java
@@ -0,0 +1,29 @@
+package com.servlets;
+/*
+ * @author Naresh Kc
+ * 
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection{
+	 Connection connects = null;	
+	public  Connection  getConnection() {
+	try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+	 	connects =DriverManager.getConnection("jdbc:mysql://localhost:3306/dci2?trueSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","root123");
+		if(connects!=null){
+			
+		}
+		else System.out.println("error123");
+
+	} catch (SQLException e) {
+		 e.getMessage();
+		e.printStackTrace();
+	} catch (Exception e) {
+
+		System.out.println("error3");
+	}
+	return connects;
+}}
