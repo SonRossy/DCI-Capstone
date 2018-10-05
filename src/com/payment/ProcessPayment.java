a6279e7a41b2bb8541394dfb0f8b9f4992d3de27
Sam Smith <41391452+magarale14@users.noreply.github.com>
2018-10-05 16:51:20 -0400

    added files

--- src/com/payment/ProcessPayment.java
+++ src/com/payment/ProcessPayment.java
@@ -2,6 +2,7 @@ package com.payment;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -33,6 +34,7 @@ public class ProcessPayment extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletRequest req = null;
 		// TODO Auto-generated method stud
 		String cname = req.getParameter("cname");
     		String ccnumber = req.getParameter("ccnumber");
