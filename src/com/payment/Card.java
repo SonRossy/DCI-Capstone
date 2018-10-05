027520176e55abb2dc02c23ba595b14129ce4e49
SonRossy <tristesse0889@hotmail.com>
2018-10-04 21:49:13 -0400

    added customer class

---
+++ src/com/payment/Card.java
@@ -0,0 +1,82 @@
+/**
+ * Author: Son-Rossy D
+ * this class add card info to customer account
+ * and charge customer 
+ */
+package com.payment;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+import com.stripe.model.Customer;
+import com.stripe.model.Token;
+
+public class Card {
+	
+	private String number;
+	private String exp_month;
+	private String exp_year;
+	private String cvc;
+	
+	
+	public Card(String number, String exp_month, String exp_year, String cvc) {
+		super();
+		this.number = number;
+		this.exp_month = exp_month;
+		this.exp_year = exp_year;
+		this.cvc = cvc;
+	}
+	
+	/**
+	 * this method returns void and take a Customer parameter to whom the card will be added
+	 * @param customer
+	 * @throws StripeException
+	 */
+	public void addCard(Customer customer) throws StripeException {
+		
+		Map<String,Object> cardParam=new HashMap<>();
+		Map<String,Object> tokenParam=new HashMap<>();
+		Map<String,Object> source=new HashMap<>();
+		
+		cardParam.put("number", number);
+		cardParam.put("exp_month", exp_month);
+		cardParam.put("exp_year", exp_year);
+		cardParam.put("cvc", cvc);
+		
+		tokenParam.put("card", cardParam);
+		
+		//now let's create a token object that will take our token parameters
+		Token token =Token.create(tokenParam);
+		
+		//token id  needs to be added to the source
+		source.put("source", token.getId());
+		
+		//at the end we need to add the source card to the customer
+		customer.getSources().create(source);
+	}
+	
+	/**
+	 * this method returns void, it charges the amount passed to the customer passed 
+	 * @param amount
+	 * @param customer
+	 */
+	public void chargeCard(String amount, Customer customer) {
+		Map<String,Object> chargeParam=new HashMap<>();
+		chargeParam.put("amount", amount);
+		chargeParam.put("currency", "usd");
+		//now we have to add customer  to be charged
+		chargeParam.put("customer", customer.getId());
+		//now it's time to initiate the charge
+		try {
+			Charge.create(chargeParam);
+			//will set message to be sent here 
+		} catch (StripeException e) {
+			//will set message to be sent here 
+			e.printStackTrace();
+		}
+	}
+	
+
+}
---
+++ src/com/payment/Customers.java
@@ -0,0 +1,18 @@
+package com.payment;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Customers {
+	private String email;
+
+	public Customers(String email) {
+		super();
+		this.email = email;
+	}
+	
+	public void createCustomer() {
+		Map<String,Object> customerParameter=new HashMap<>();
+	}
+
+}
