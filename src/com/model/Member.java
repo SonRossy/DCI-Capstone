7816f6051583bae541537bea365e9dff7ae531d8
Noel Cortes <noelcortes472640@gmail.com>
2018-11-01 00:24:07 -0400

    updatePassword validation added

--- src/com/model/Member.java
+++ src/com/model/Member.java
@@ -1,4 +1,4 @@
-package com.model;
+package com.model; 
 /*
  * author Clarissa Mercado
  * Created Member class to associate attributes
--- src/com/servlets/UpdatePasswordServlet.java
+++ src/com/servlets/UpdatePasswordServlet.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -61,32 +62,49 @@ public class UpdatePasswordServlet extends HttpServlet {
 		String newPassword = request.getParameter("newPassword").trim();
 		String confirmNewPassword = request.getParameter("confirmNewPassword").trim();
 		
+		/*Start: Author of back-end validation: Noel Cortes */ 
+		boolean regexNewPassword = Pattern.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$", newPassword);
+			System.out.println(regexNewPassword);
+			System.out.println(newPassword.length());
+
+		
 		try(PreparedStatement pst = connection.prepareStatement(query)) {
-			if(newPassword.equals(confirmNewPassword)) {
+			if(regexNewPassword &&
+			  (newPassword.length()>8 && newPassword.length()<16) &&
+			   confirmNewPassword.equals(newPassword)) {
+				
 				pst.setString(1, newPassword);
 				pst.setString(2, email);
 				pst.setString(3, oldPassword);
-			}
-			
-			if(pst.executeUpdate()!=0) {
-				System.out.println("New password successfully saved into the database!");	
-				message = "Password successfully updated!";
+				
+				if(pst.executeUpdate()!=0) {
+					System.out.println("New password successfully saved into the database!");	
+					message = "Password successfully updated!";
+					request.setAttribute("message", message);
+					RequestDispatcher dispatcher = request.getRequestDispatcher("customerProfile.jsp");
+					dispatcher.forward(request, response);
+				} else {
+					message = "Invalid password(s). Please follow password requirements and try again.";
+					request.setAttribute("message", message);
+					RequestDispatcher dispatcher = request.getRequestDispatcher("customerProfile.jsp");
+					dispatcher.forward(request, response);			
+				}
+			}else {
+				System.out.println("Invalid password: update password validation requirements not met.");
+				message = "Invalid password(s). Please follow password requirements and try again.";
 				request.setAttribute("message", message);
 				RequestDispatcher dispatcher = request.getRequestDispatcher("customerProfile.jsp");
 				dispatcher.forward(request, response);
-			} else {
-				message = "Invalid password(s). Please try again.";
-				request.setAttribute("message", message);
-				RequestDispatcher dispatcher = request.getRequestDispatcher("customerProfile.jsp");
-				dispatcher.forward(request, response);			
 			}
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
-			message = "Invalid password(s). Please try again.";
+			message = "Invalid password(s). Please follow password requirements and try again.";
 			request.setAttribute("message", message);
 			request.setAttribute("passwordTruncate", e.getMessage());
 			RequestDispatcher dispatcher = request.getRequestDispatcher("customerProfile.jsp");
-			dispatcher.forward(request, response);		}		
+			dispatcher.forward(request, response);		
+		}
+		/*End: Author of back-end validation: Noel Cortes */ 
 	}
 }
